04b23e6557b118f33d4c3fed43ef87f813fbdd5a
Sharath19012002 <115027108+Sharath19012002@users.noreply.github.com>
2023-12-28 16:01:17 +0530

    Delete ASSIGNMENT-3 directory

--- ASSIGNMENT-3/DBPropertyUtil.py
+++
@@ -1,7 +0,0 @@
-class PropUtil:
-    def getPropertyString(self,host,username,password,database):
-        host = 'localhost'
-        username = 'root'
-        password = 'root'
-        database = 'ticketbookingsystem'
-        return host, username, password, database
