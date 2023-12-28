c1074e395b10b7ea99f39580cc6e04b4017085de
Sharath19012002 <115027108+Sharath19012002@users.noreply.github.com>
2023-12-28 16:02:21 +0530

    Add files via upload

---
+++ PYTHON ASSIGNMENT-5/DBPropertyUtil.py
@@ -0,0 +1,7 @@
+class PropUtil:
+    def getPropertyString(self,host,username,password,database):
+        host = 'localhost'
+        username = 'root'
+        password = 'root'
+        database = 'ticketbookingsystem'
+        return host, username, password, database
